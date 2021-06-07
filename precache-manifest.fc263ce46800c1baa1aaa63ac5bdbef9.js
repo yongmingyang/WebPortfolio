5bb8dc7a73d87e118fd36e7ab281111ffcf28765
yongmingyang <yongmingyang1998@gmail.com>
2021-06-07 23:54:04 +0800

    Updates

---
+++ precache-manifest.fc263ce46800c1baa1aaa63ac5bdbef9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dec448cf0f8fc053ab3ca5c2a91d816b",
+    "url": "/WebPortfolio/index.html"
+  },
+  {
+    "revision": "47c089ba40f2936fa972",
+    "url": "/WebPortfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f04aae6469ebabbb895c",
+    "url": "/WebPortfolio/static/js/2.a0363d32.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/WebPortfolio/static/js/2.a0363d32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47c089ba40f2936fa972",
+    "url": "/WebPortfolio/static/js/main.88727ae9.chunk.js"
+  },
+  {
+    "revision": "7d7e1d2b66d2115c270f",
+    "url": "/WebPortfolio/static/js/runtime-main.d011e4e3.js"
+  },
+  {
+    "revision": "1d3037b506dc5762cc492760aedfcf8d",
+    "url": "/WebPortfolio/static/media/AER.1d3037b5.png"
+  },
+  {
+    "revision": "b4be90e6beb41368440f1f64ba2b0700",
+    "url": "/WebPortfolio/static/media/GoogleApsScript.b4be90e6.png"
+  },
+  {
+    "revision": "b0df7f1886fa3eaff2ce18e89c6707a8",
+    "url": "/WebPortfolio/static/media/InstaWear.b0df7f18.jpg"
+  },
+  {
+    "revision": "9b7b4f44991f76f2d4386d0578df4026",
+    "url": "/WebPortfolio/static/media/InstawearPoster.9b7b4f44.jpg"
+  },
+  {
+    "revision": "ddce654f957844610f5ee96ee9881458",
+    "url": "/WebPortfolio/static/media/M.ddce654f.jpg"
+  },
+  {
+    "revision": "ffdfd06ab17e226bd7136efb27be9d65",
+    "url": "/WebPortfolio/static/media/Mynorca_example.ffdfd06a.PNG"
+  },
+  {
+    "revision": "16846ebfe08e5195d124f530e93b80bf",
+    "url": "/WebPortfolio/static/media/bulk_upload.16846ebf.PNG"
+  },
+  {
+    "revision": "b15de7f200b887c142d99b922ef30a43",
+    "url": "/WebPortfolio/static/media/displayphoto.b15de7f2.jpg"
+  },
+  {
+    "revision": "f50e7a3ed6739d213cf804f51bfc7791",
+    "url": "/WebPortfolio/static/media/login.f50e7a3e.PNG"
+  },
+  {
+    "revision": "2f414ab13b4c9f6faf39ddd30852b341",
+    "url": "/WebPortfolio/static/media/marketplace.2f414ab1.PNG"
+  },
+  {
+    "revision": "22ff3f4f90b978e976759d0da9466171",
+    "url": "/WebPortfolio/static/media/marketplace_explanation.22ff3f4f.PNG"
+  }
+]);
