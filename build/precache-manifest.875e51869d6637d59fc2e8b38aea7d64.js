0ac6a085e9633e81fcbed9b1e906289be82795ef
yongmingyang <yongmingyang1998@gmail.com>
2021-06-07 23:18:13 +0800

    build folder

---
+++ build/precache-manifest.875e51869d6637d59fc2e8b38aea7d64.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63f2f797383eb838b5ac9154b44f24c4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eab7bf63ee99e4ca0b3",
+    "url": "/static/css/main.dc0a14bb.chunk.css"
+  },
+  {
+    "revision": "63f05d9af4a24cf90343",
+    "url": "/static/js/2.80ca9cec.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.80ca9cec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eab7bf63ee99e4ca0b3",
+    "url": "/static/js/main.2aedc210.chunk.js"
+  },
+  {
+    "revision": "d9f4075f89abc1aeda28",
+    "url": "/static/js/runtime-main.ca96b7af.js"
+  },
+  {
+    "revision": "1d3037b506dc5762cc492760aedfcf8d",
+    "url": "/static/media/AER.1d3037b5.png"
+  },
+  {
+    "revision": "b4be90e6beb41368440f1f64ba2b0700",
+    "url": "/static/media/GoogleApsScript.b4be90e6.png"
+  },
+  {
+    "revision": "b0df7f1886fa3eaff2ce18e89c6707a8",
+    "url": "/static/media/InstaWear.b0df7f18.jpg"
+  },
+  {
+    "revision": "9b7b4f44991f76f2d4386d0578df4026",
+    "url": "/static/media/InstawearPoster.9b7b4f44.jpg"
+  },
+  {
+    "revision": "ddce654f957844610f5ee96ee9881458",
+    "url": "/static/media/M.ddce654f.jpg"
+  },
+  {
+    "revision": "ffdfd06ab17e226bd7136efb27be9d65",
+    "url": "/static/media/Mynorca_example.ffdfd06a.PNG"
+  },
+  {
+    "revision": "16846ebfe08e5195d124f530e93b80bf",
+    "url": "/static/media/bulk_upload.16846ebf.PNG"
+  },
+  {
+    "revision": "b15de7f200b887c142d99b922ef30a43",
+    "url": "/static/media/displayphoto.b15de7f2.jpg"
+  },
+  {
+    "revision": "f50e7a3ed6739d213cf804f51bfc7791",
+    "url": "/static/media/login.f50e7a3e.PNG"
+  },
+  {
+    "revision": "2f414ab13b4c9f6faf39ddd30852b341",
+    "url": "/static/media/marketplace.2f414ab1.PNG"
+  },
+  {
+    "revision": "22ff3f4f90b978e976759d0da9466171",
+    "url": "/static/media/marketplace_explanation.22ff3f4f.PNG"
+  }
+]);
